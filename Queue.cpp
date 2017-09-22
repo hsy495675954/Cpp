2cae402a3a16c01276379ae7bd18e92101207b0c
hsy495675954 <495675954@qq.com>
2017-09-22 23:57:49 +0800

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,72 @@
+#ifndef QUEUE_CPP
+#define QUEUE_CPP
+
+#include <iostream>
+using namespace std;
+
+template <typename T> 
+class Queue
+{
+	private:
+		struct Node
+		{
+			T Data;
+			Node *Next;
+		};
+		
+		
+		Node Head;
+		Node *Final;
+		int count;
+	
+	public:
+		Queue()
+		{
+			Head.Next = NULL;
+			Final = &Head;
+			count = 0;
+		}
+		
+		void Enqueue(T data)
+		{
+			Final->Next = new Node;
+			Final = Final->Next;
+			Final->Data = data;
+			Final->Next = NULL;
+			
+			count++;
+		}
+		
+		void Dequeue()
+		{
+			if (count == 0)
+			{
+				cout << "Is empty!" << endl;
+				return;
+			}
+			
+			Node *Temp = Head.Next;
+			Head.Next = Head.Next->Next;
+			
+			delete Temp;
+			count--;
+		}
+		
+		T GetFirstData()
+		{
+			return Head.Next->Data;
+		}
+		
+		T GetFinalData()
+		{
+			return Final->Data;
+		}
+		
+		int Num_Of_Nodes()
+		{
+			return count;
+		}	
+		
+};
+		
+#endif
